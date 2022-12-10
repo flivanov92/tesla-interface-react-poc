5aea2a9185417c7f0fe6d282599b9f2d1e1614f6
Alex <flivanov92@gmail.com>
2022-12-10 09:47:50 +0000

    #9 create background component

--- src/component/background/background.jsx
+++ src/component/background/background.jsx
@@ -1,14 +1,29 @@
 import * as S from './background.style';
-import { node, string } from 'prop-types';
+import { shape, node, string, arrayOf } from 'prop-types';
 
-const Background = ({ children, id }) => {
-  return <S.Background image={id}>{children}</S.Background>;
+const Background = ({ section }) => {
+  const { id, link, who, buttonList } = section;
+  return <S.Background image={id}>{who}</S.Background>;
 };
 
 Background.defaultProps = {
-  children: node,
-  name: string,
-  id: string.isRequired,
+  section: {
+    id: '',
+    link: '',
+    text: '',
+    who: '',
+    buttonList: [],
+  },
+};
+
+Background.protoTypes = {
+  section: shape({
+    children: node,
+    id: string.isRequired,
+    link: string.isRequired,
+    text: string.isRequired,
+    buttonList: arrayOf(string),
+  }),
 };
 
 export default Background;
--- src/component/tesla/tesla.jsx
+++ src/component/tesla/tesla.jsx
@@ -1,16 +1,42 @@
+import { useEffect, useState } from 'react';
 import { Background, Global, Theme } from '..';
 
-const Tesla = () => (
-  <div>
-    <Theme>
-      <Global />
-      <h1>Hi there</h1>
-      <br></br>
-      <br></br>
-      <p>Yeeeyas </p>
-      <Background id='model-3'>yyyy</Background>
-    </Theme>
-  </div>
-);
+const Tesla = () => {
+  const [mylist, update] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const result = await (
+          await fetch('http://localhost:3001/section')
+        ).json();
+
+        update(result);
+      } catch (error) {
+        console.error(
+          `Error with message: ${error.message} and code: ${error.code}`
+        );
+      }
+    })();
+  }, []);
+
+  console.log(mylist);
+
+  return (
+    <div>
+      <Theme>
+        <Global />
+
+        <h1>Hi there</h1>
+        <br></br>
+        <br></br>
+        <p>Yeeeyas </p>
+        {mylist.map((section) => (
+          <Background key={section.id} section={section} />
+        ))}
+      </Theme>
+    </div>
+  );
+};
 
 export default Tesla;
